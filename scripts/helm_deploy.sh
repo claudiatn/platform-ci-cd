221636c805a7cf3dc8619ad261c6527ae875439c
claudiatn <cntenorio@stemdo.io>
2026-01-20 11:39:31 +0100

    script deploy helm

---
+++ scripts/helm_deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+NAMESPACE="$1"
+ENVIRONMENT="$2"
+ACTOR="$3"
+GHCR_PAT="$4"
+CHART="$5"
+
+BUILD_CONTEXT="${GITHUB_WORKSPACE}"
+
+echo "CREATE NAMESPACE"
+kubectl create namespace "$NAMESPACE"-"$ENVIRONMENT"
+
+
+echo "CREATE GHCR-SECRET"
+kubectl create secret docker-registry ghcr-secret \
+            --docker-server=ghcr.io \
+            --docker-username=$ACTOR \
+            --docker-password=$GHCR_PAT \
+            --namespace="$NAMESPACE"-"$ENVIRONMENT"
+
+echo "DEPLOY HELM"
+helm upgrade --install $CHART ./helm \
+             --namespace "$NAMESPACE"-"$ENVIRONMENT"
+                         
